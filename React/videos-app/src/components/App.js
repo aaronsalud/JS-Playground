6dc08911de452d035a040c1e4226c9ca1cfffa99
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-12-15 22:02:11 -0500

    Refactor callback function

--- React/videos-app/src/components/App.js
+++ React/videos-app/src/components/App.js
@@ -25,10 +25,6 @@ const App = () => {
         setSelectedVideo(response.data.items[0] );
     }
 
-    const onVideoSelect = (video) => {
-        setSelectedVideo(video);
-    }
-
     return (
         <div className="ui container" style={{ marginTop: '10px' }}>
             <SearchBar onFormSubmit={onSearchSubmit} />
@@ -38,7 +34,7 @@ const App = () => {
                         <VideoDetail video={selectedVideo} />
                     </div>
                     <div className="five wide column">
-                        <VideoList videos={videos} onVideoSelect={onVideoSelect} />
+                        <VideoList videos={videos} onVideoSelect={setSelectedVideo} />
                     </div>
                 </div>
             </div>
