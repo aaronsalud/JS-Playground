d37f64307820c92401b60a5997e557072d53fbc3
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-12-16 20:04:15 -0500

    Fix layout

--- React/songs-app/src/components/App.js
+++ React/songs-app/src/components/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import SongList from './SongList';
 
 const App = () => {
-    return(
-        <div>App</div>
+    return (
+        <div className="ui container grid" style={{marginTop:'20px'}}>
+            <div className="ui row">
+                <div className="column eight wide">
+                    <SongList />
+                </div>
+            </div>
+        </div>
     );
 };
 
