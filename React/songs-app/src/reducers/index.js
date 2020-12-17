9572303d10d253e11f5569eaa02947113e3ae986
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-12-16 19:14:21 -0500

    Add reducers

---
+++ React/songs-app/src/reducers/index.js
@@ -0,0 +1,16 @@
+const songsReducer = () =>{
+    return [
+        { title: 'High Fashion', duration: '4:05' },
+        { title: 'Fortress', duration: '3:10' },
+        { title: 'Blueberry Faygo', duration: '5:09' },
+        { title: 'Down Below', duration: '4:15' }
+    ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+    if(action === 'SONG_SELECTED'){
+        return action.payload;
+    }
+
+    return selectedSong;
+};
