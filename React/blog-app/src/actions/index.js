82caf4d9aa09288e28cca4faf8345f6b823a024b
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-12-17 23:19:00 -0500

    Add private method to apply memoize and avoid multiple API calls with the same endpoint

--- React/blog-app/src/actions/index.js
+++ React/blog-app/src/actions/index.js
@@ -1,11 +1,16 @@
 import axios from '../api/axios';
+import _ from 'lodash';
 
 export const fetchPosts = () => async dispatch => {
     const response = await axios.get('/posts');
     dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-export const fetchUser = (id) => async dispatch => {
-    const response = await axios.get(`/users/${id}`);
-    dispatch({type: 'FETCH_USER', payload: response.data});
+export const fetchUser = (id) => dispatch => {
+    _fetchUser(id, dispatch);
 };
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
+    const response = await axios.get(`/users/${id}`);
+    dispatch({ type: 'FETCH_USER', payload: response.data });
+});
