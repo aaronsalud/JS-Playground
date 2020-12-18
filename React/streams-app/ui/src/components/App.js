36f6b671cafe040393f7d1dccdf09d41b2bf67f3
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-12-18 18:22:56 -0500

    Render Header component

--- React/streams-app/ui/src/components/App.js
+++ React/streams-app/ui/src/components/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+
+import Header from './Header';
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
@@ -9,13 +11,16 @@ import StreamShow from './streams/StreamShow';
 
 const App = () => {
     return (
-        <div>
+        <div className="ui">
             <BrowserRouter>
-                <Route path="/" exact component={StreamList} />
-                <Route path="/streams/new" exact component={StreamCreate} />
-                <Route path="/streams/edit" exact component={StreamEdit} />
-                <Route path="/streams/delete" exact component={StreamDelete} />
-                <Route path="/streams/show" exact component={StreamShow} />
+                <Header />
+                <div className="ui container">
+                    <Route path="/" exact component={StreamList} />
+                    <Route path="/streams/new" exact component={StreamCreate} />
+                    <Route path="/streams/edit" exact component={StreamEdit} />
+                    <Route path="/streams/delete" exact component={StreamDelete} />
+                    <Route path="/streams/show" exact component={StreamShow} />
+                </div>
             </BrowserRouter>
         </div>
     );
