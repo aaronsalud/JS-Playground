f83183867f40ce17defaed7edaf3911f43ab88d7
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-12-19 16:19:59 -0500

    Load Google Auth instance

--- React/streams-app/ui/src/components/GoogleAuth.js
+++ React/streams-app/ui/src/components/GoogleAuth.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 
 class GoogleAuth extends Component {
-    componentDidMount(){
-        window.gapi.load('client:auth2', ()=>{
+    state = { isSignedIn: null };
+    componentDidMount() {
+        window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
-               clientId: '731848874698-g0unv8i340gppcdg8r4v2lgp2m19eck6.apps.googleusercontent.com',
-               scope: 'email' 
+                clientId: '731848874698-g0unv8i340gppcdg8r4v2lgp2m19eck6.apps.googleusercontent.com',
+                scope: 'email'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({ isSignedIn: this.auth.isSignedIn.get() });
             });
         });
     }
