82185656407aec81b4199c5321b292f1df141bd1
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-12-20 19:27:53 -0500

    Add form submit handler

--- React/streams-app/ui/src/components/streams/StreamCreate.js
+++ React/streams-app/ui/src/components/streams/StreamCreate.js
@@ -5,18 +5,24 @@ class StreamCreate extends Component {
 
     renderInput({ input, label }) {
         return (
-           <div className="field">
-               <label htmlFor="">{label}</label>
+            <div className="field">
+                <label htmlFor="">{label}</label>
                 <input {...input} />
-           </div>
+            </div>
         );
     }
 
+    onSubmit(formValues) {
+        console.log(formValues);
+    }
+
     render() {
+        const { handleSubmit } = this.props;
         return (
-            <form className="ui form">
+            <form onSubmit={handleSubmit(this.onSubmit)} className="ui form">
                 <Field name="title" label="Title" component={this.renderInput} />
                 <Field name="description" label="Description" component={this.renderInput} />
+                <button className="ui button primary">Submit</button>
             </form>
         );
     }
