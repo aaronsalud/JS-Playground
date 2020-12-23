5e552689438755493e4eaf6e43db9aa1238bba50
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-12-23 15:55:44 -0500

    Add action creator method to delete a stream

--- React/streams-app/ui/src/actions/index.js
+++ React/streams-app/ui/src/actions/index.js
@@ -35,3 +35,7 @@ export const editStream = (id, formValues) => async dispatch => {
     dispatch({ type: EDIT_STREAM, payload: response.data });
 };
 
+export const deleteStream = (id) => async dispatch => {
+    await axios.delete(`/streams/${id}`);
+    dispatch({ type: DELETE_STREAM, payload: id });
+};
