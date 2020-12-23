233e929f84401e1cfb174fd2988745b0bf3391a9
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-12-23 16:47:54 -0500

    Add create stream button in streams list page

--- React/streams-app/ui/src/components/streams/StreamList.js
+++ React/streams-app/ui/src/components/streams/StreamList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchStreams } from '../../actions/';
 
 class StreamList extends Component {
@@ -10,7 +11,7 @@ class StreamList extends Component {
 
     renderActionButtons(stream) {
         if (stream.userId === this.props.currentUserId) {
-            return(
+            return (
                 <div className="right floated content">
                     <button className="ui button primary">Edit</button>
                     <button className="ui button negative">Delete</button>
@@ -34,10 +35,21 @@ class StreamList extends Component {
         });
     }
 
+    renderCreateButton() {
+        if (this.props.isSignedIn) {
+            return(
+                <div style={{textAlign:'right'}}>
+                    <Link to="/streams/new" className="ui button primary">Create Stream</Link>
+                </div>
+            );
+        }
+    }
+
     render() {
         return (
             <div>
                 <h2>Streams</h2>
+                {this.renderCreateButton()}
                 <div className="ui celled list">
                     {this.renderList()}
                 </div>
@@ -50,7 +62,8 @@ class StreamList extends Component {
 const mapStateToProps = (state) => {
     return {
         streams: state.streams,
-        currentUserId: state.auth.userId
+        currentUserId: state.auth.userId,
+        isSignedIn: state.auth.isSignedIn
     };
 };
 
