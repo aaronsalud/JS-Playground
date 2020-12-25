dae7e6b525d162259ffa9f5ea3712238823ee5d0
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-12-25 16:12:46 -0500

    Use patch request instead of put when editing a stream

--- React/streams-app/ui/src/actions/index.js
+++ React/streams-app/ui/src/actions/index.js
@@ -34,7 +34,7 @@ export const createStream = formValues => async (dispatch, getState) => {
 
 
 export const editStream = (id, formValues) => async dispatch => {
-    const response = await axios.put(`/streams/${id}`, formValues);
+    const response = await axios.patch(`/streams/${id}`, formValues);
     dispatch({ type: EDIT_STREAM, payload: response.data });
     history.push('/');
 };
