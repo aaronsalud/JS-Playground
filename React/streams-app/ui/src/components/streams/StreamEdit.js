7489de17e8a42c6861c7953bfba3395fa6ffc55a
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-12-25 16:05:24 -0500

    Filter out title and description from form payload

--- React/streams-app/ui/src/components/streams/StreamEdit.js
+++ React/streams-app/ui/src/components/streams/StreamEdit.js
@@ -1,14 +1,14 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchStream, clearSelectedStream, editStream } from '../../actions';
 import StreamForm from './StreamForm';
 
-
 class StreamEdit extends Component {
 
     onSubmit = (formValues) => {
         const streamId = this.props.match.params.id;
-        this.props.editStream(streamId, formValues);
+        this.props.editStream(streamId, _.pick(formValues, 'title', 'description'));
     }
 
     componentDidMount() {
