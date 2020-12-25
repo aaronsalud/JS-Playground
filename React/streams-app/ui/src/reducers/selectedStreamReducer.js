bc6faeb8d14abdc30aebad081ef176c04dc39e12
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-12-25 15:16:57 -0500

    Add functionality to clear selected stream

--- React/streams-app/ui/src/reducers/selectedStreamReducer.js
+++ React/streams-app/ui/src/reducers/selectedStreamReducer.js
@@ -1,9 +1,11 @@
-import { FETCH_STREAM } from '../actions/types';
+import { FETCH_STREAM, CLEAR_SELECTED_STREAM } from '../actions/types';
 
 const selectedStreamReducer = (state = null, action) => {
     switch (action.type) {
         case FETCH_STREAM:
             return action.payload;
+        case CLEAR_SELECTED_STREAM:
+            return null;
         default:
             return state;
     }
