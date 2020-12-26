7d5ddfaa8b14cbc68383421e39a30875384e861e
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-12-26 15:32:35 -0500

    Display stream content in display stream page

--- React/streams-app/ui/src/components/streams/StreamShow.js
+++ React/streams-app/ui/src/components/streams/StreamShow.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchStream, clearSelectedStream } from '../../actions';
 
-const StreamShow = () => {
-    return (
-        <div>StreamShow</div>
-    );
-};
+class StreamShow extends Component {
 
-export default StreamShow;
+    componentDidMount() {
+        const streamId = this.props.match.params.id;
+        this.props.fetchStream(streamId);
+    }
+
+    componentWillUnmount() {
+        this.props.clearSelectedStream();
+    }
+
+    render() {
+        if (!this.props.stream) {
+            return <div>Loading...</div>
+        }
+
+        const { title, description } = this.props.stream;
+        return (
+            <div>
+                <h1>{title}</h1>
+                <h5>{description}</h5>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        stream: state.selectedStream
+    }
+}
+
+export default connect(mapStateToProps, { fetchStream, clearSelectedStream })(StreamShow);
