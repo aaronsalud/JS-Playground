f4c65846cbc71c159a11eee1033b8d14733b9513
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-12-26 14:32:59 -0500

    Add onDismiss prop to modal

--- React/streams-app/ui/src/components/Modal.js
+++ React/streams-app/ui/src/components/Modal.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import history from '../history';
 
-const Modal = ({ title, description, actions }) => {
+const Modal = ({ title, description, actions, onDismiss }) => {
     return ReactDOM.createPortal(
-        <div onClick={() => history.push('/')} className="ui dimmer modals visible active">
+        <div onClick={onDismiss} className="ui dimmer modals visible active">
             <div onClick={e => e.stopPropagation()} className="ui standard modal visible active">
                 <div className="header">{title}</div>
                 <div className="content">{description}</div>
