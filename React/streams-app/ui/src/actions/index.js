9833a2154b2b41a2504fc2e77cf8eb34178304c5
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-12-26 15:04:28 -0500

    Redirect back to stream list page after deleting a stream

--- React/streams-app/ui/src/actions/index.js
+++ React/streams-app/ui/src/actions/index.js
@@ -42,6 +42,7 @@ export const editStream = (id, formValues) => async dispatch => {
 export const deleteStream = (id) => async dispatch => {
     await axios.delete(`/streams/${id}`);
     dispatch({ type: DELETE_STREAM, payload: id });
+    history.push('/');
 };
 
 export const clearSelectedStream = () => ({ type: CLEAR_SELECTED_STREAM });
