d03c6831f40fd1560bdf15a19445bcaa2f20486d
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-12-26 14:22:59 -0500

    Render modal to delete a stream

--- React/streams-app/ui/src/components/streams/StreamDelete.js
+++ React/streams-app/ui/src/components/streams/StreamDelete.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Modal from '../Modal';
 
 const StreamDelete = () => {
+
+    const modalTitle = 'Delete Stream';
+    const modalDescription = 'Are you sure you want to delete this stream?';
+
+    const modalActions = (
+        <Fragment>
+            <button className="ui negative button">Delete</button>
+            <button className="ui button">Cancel</button>
+        </Fragment>
+    );
+    
     return (
-        <div>StreamDelete</div>
+        <Modal title={modalTitle} description={modalDescription} actions={modalActions} />
     );
 };
 
