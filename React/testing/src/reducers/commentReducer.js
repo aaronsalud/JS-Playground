bafba68070d1cccab3827ce4a58eaf7cb336eb6c
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-12-29 14:10:35 -0500

    Add action creator and reducer to add coment

--- React/testing/src/reducers/commentReducer.js
+++ React/testing/src/reducers/commentReducer.js
@@ -1,5 +1,9 @@
+import { ADD_COMMENT } from 'actions/types';
+
 const commentReducer = (state = [], action) => {
     switch (action.type) {
+        case ADD_COMMENT:
+            return [...state, action.payload];
         default:
             return state;
     }
