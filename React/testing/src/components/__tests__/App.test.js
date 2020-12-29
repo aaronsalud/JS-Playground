a041839cffc8ac681e12fc6d565aa46fcf4adbb3
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-12-29 11:44:44 -0500

    Add test to check for CommentList component within App component

--- React/testing/src/components/__tests__/App.test.js
+++ React/testing/src/components/__tests__/App.test.js
@@ -2,9 +2,15 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../App';
 import CommentBox from '../CommentBox';
+import CommentList from '../CommentList';
 
 it('contains a CommentBox component', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find(CommentBox).length).toEqual(1);
+});
+
+it('contains a CommentList component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(CommentList).length).toEqual(1);
 });
 
