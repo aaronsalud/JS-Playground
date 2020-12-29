ff3e814dfe17aed2cf18e5d75641c264c9ce1946
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-12-29 13:02:28 -0500

    Add test for form submit

--- React/testing/src/components/__tests__/CommentBox.test.js
+++ React/testing/src/components/__tests__/CommentBox.test.js
@@ -20,9 +20,22 @@ it('has a text area and a button', () => {
 it('has a text area that users can type in', () => {
     // Simulate change event
     wrapper.find('textarea').simulate('change', {
-        target: { value: 'new comment'}
+        target: { value: 'new comment' }
     });
     wrapper.update(); // Force component to re render
 
     expect(wrapper.find('textarea').prop('value')).toEqual('new comment'); // Expect textarea value prop to contain new value set from change event
+});
+
+it('should clear the textarea when form is submitted', () => {
+    wrapper.find('textarea').simulate('change', {
+        target: { value: 'new comment'}
+    });
+
+    wrapper.update();
+
+    wrapper.find('form').simulate('submit');
+    wrapper.update();
+
+    expect(wrapper.find('textarea').prop('value')).toEqual('');
 });
