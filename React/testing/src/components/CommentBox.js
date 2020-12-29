21c72a1d47d2f10cf3a1377c9aec8002bf155435
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-12-29 12:03:12 -0500

    Add textarea event handler

--- React/testing/src/components/CommentBox.js
+++ React/testing/src/components/CommentBox.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 
 class CommentBox extends Component {
 
+    state = { comment: '' };
+
     render() {
         return (
             <form action="">
                 <h4>Add a comment</h4>
-                <textarea name="" id="" cols="30" rows="10"></textarea>
+                <textarea name="" id="" cols="30" rows="10" value={this.state.comment} onChange={(e) => this.setState({ comment: e.target.value })}></textarea>
                 <div>
                     <button>Submit a comment</button>
                 </div>
