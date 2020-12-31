9e0384919f9d1776ddb94b3273e2dc2a07d00caf
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-12-31 14:19:23 -0500

    Add action creator to fetch comments from jsonplaceholder

--- React/testing/src/actions/index.js
+++ React/testing/src/actions/index.js
@@ -1,8 +1,18 @@
-import { ADD_COMMENT } from 'actions/types';
+import axios from 'axios';
+import { ADD_COMMENT, FETCH_COMMENTS } from 'actions/types';
 
 export const addComment = (comment) => {
     return {
         type: ADD_COMMENT,
         payload: comment
+    }
+}
+
+export const fetchComments = () => {
+    const response = axios.get('http://jsonplaceholder.typicode.com/comments');
+
+    return {
+        type: FETCH_COMMENTS,
+        payload: response
     }
 }
