2ad39ccf3bedb3f9584921002db50dea13edbf92
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-12-31 13:55:24 -0500

    Modify root component to generate initialState from props used for testing

--- React/testing/src/Root.js
+++ React/testing/src/Root.js
@@ -6,8 +6,13 @@ import reducers from 'reducers';
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducers, composeEnhancers(applyMiddleware()));
 
+const initializeStore = (initialState = null) => {
+    // If there is an initialState (used for testing) provided generate the store with it
+    return initialState ? createStore(reducers, initialState, composeEnhancers(applyMiddleware())) : createStore(reducers, composeEnhancers(applyMiddleware()));
+};
+
 const root = (props) => {
-   return <Provider store={store}>{props.children}</Provider>;
+    return <Provider store={initializeStore(props.initialState)}>{props.children}</Provider>;
 };
 
 export default root;
