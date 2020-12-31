1599dec251e343344bd927334b885338b95c7837
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-12-31 13:58:11 -0500

    Refactor with props setting default value of state

--- React/testing/src/Root.js
+++ React/testing/src/Root.js
@@ -6,13 +6,8 @@ import reducers from 'reducers';
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducers, composeEnhancers(applyMiddleware()));
 
-const initializeStore = (initialState = null) => {
-    // If there is an initialState (used for testing) provided generate the store with it
-    return initialState ? createStore(reducers, initialState, composeEnhancers(applyMiddleware())) : createStore(reducers, composeEnhancers(applyMiddleware()));
-};
-
-const root = (props) => {
-    return <Provider store={initializeStore(props.initialState)}>{props.children}</Provider>;
+const root = ({children, initialState = {}}) => {
+    return <Provider store={createStore(reducers, initialState, composeEnhancers(applyMiddleware()))}>{children}</Provider>;
 };
 
 export default root;
