a50fe5eae5a5414f79b654d8714e528467437e9a
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-12-31 14:25:31 -0500

    Handle state update when comments have been fetched

--- React/testing/src/reducers/commentsReducer.js
+++ React/testing/src/reducers/commentsReducer.js
@@ -1,7 +1,10 @@
-import { ADD_COMMENT } from 'actions/types';
+import { ADD_COMMENT, FETCH_COMMENTS } from 'actions/types';
 
 const commentsReducer = (state = [], action) => {
     switch (action.type) {
+        case FETCH_COMMENTS:
+            const comments = action.payload.data.map(comment => comment.name);
+            return [...state, ...comments];
         case ADD_COMMENT:
             return [...state, action.payload];
         default:
