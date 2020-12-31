2e2d978e06e32bc1e9a3ae3660703b66ba71a069
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-12-31 14:25:01 -0500

    Add button to fetch comments from jsonplaceholder

--- React/testing/src/components/CommentBox.js
+++ React/testing/src/components/CommentBox.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { addComment } from 'actions';
+import { addComment, fetchComments } from 'actions';
 
 class CommentBox extends Component {
 
@@ -14,15 +14,19 @@ class CommentBox extends Component {
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <h4>Add a comment</h4>
-                <textarea name="" id="" cols="30" rows="10" value={this.state.comment} onChange={(e) => this.setState({ comment: e.target.value })}></textarea>
-                <div>
-                    <button>Submit a comment</button>
-                </div>
-            </form>
+            <div>
+
+                <form onSubmit={this.handleSubmit}>
+                    <h4>Add a comment</h4>
+                    <textarea name="" id="" cols="30" rows="10" value={this.state.comment} onChange={(e) => this.setState({ comment: e.target.value })}></textarea>
+                    <div>
+                        <button>Submit a comment</button>
+                    </div>
+                </form>
+                <button onClick={this.props.fetchComments}>Fetch Comments</button>
+            </div>
         );
     }
 }
 
-export default connect(null, { addComment })(CommentBox);
+export default connect(null, { addComment, fetchComments })(CommentBox);
