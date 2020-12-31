f995bcd4f51da028c22b1897826961754cc96ff1
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-12-31 14:19:53 -0500

    Setup redux-promise middleware

--- React/testing/src/Root.js
+++ React/testing/src/Root.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, compose, applyMiddleware } from 'redux';
+import reduxPromise from 'redux-promise';
 import reducers from 'reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, composeEnhancers(applyMiddleware()));
 
-const root = ({children, initialState = {}}) => {
-    return <Provider store={createStore(reducers, initialState, composeEnhancers(applyMiddleware()))}>{children}</Provider>;
+const root = ({ children, initialState = {} }) => {
+    const store = createStore(reducers, initialState, composeEnhancers(applyMiddleware(reduxPromise)));
+    return <Provider store={store}>{children}</Provider>;
 };
 
 export default root;
