dc98703d2ea0bbc7c827209403deadc2fafb9bfa
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-01 14:27:20 -0500

    Add action creator to change isLoggedIn state

--- React/hoc-example/src/actions/index.js
+++ React/hoc-example/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { ADD_COMMENT, FETCH_COMMENTS } from 'actions/types';
+import { ADD_COMMENT, FETCH_COMMENTS, CHANGE_AUTH } from 'actions/types';
 
 export const addComment = (comment) => {
     return {
@@ -16,3 +16,10 @@ export const fetchComments = () => {
         payload: response
     }
 }
+
+export const changeAuth = (isLoggedIn) => {
+    return {
+        type: CHANGE_AUTH,
+        payload: isLoggedIn
+    }
+};
