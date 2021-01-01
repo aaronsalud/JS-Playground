f81d6699c667019a7e8c530e3b88805977e2b8cc
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-01 18:51:49 -0500

    Configure asyncMiddleware

--- React/middlewares-example/src/Root.js
+++ React/middlewares-example/src/Root.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, compose, applyMiddleware } from 'redux';
-import reduxPromise from 'redux-promise';
 import reducers from 'reducers';
+import asyncMiddleware from 'middlewares/asyncMiddleware';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const root = ({ children, initialState = {} }) => {
-    const store = createStore(reducers, initialState, composeEnhancers(applyMiddleware(reduxPromise)));
+    const store = createStore(reducers, initialState, composeEnhancers(applyMiddleware(asyncMiddleware)));
     return <Provider store={store}>{children}</Provider>;
 };
 
