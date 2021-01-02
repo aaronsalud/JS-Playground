876d64b06122c54b1c7faca7f7095adc2421d4c9
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-01 19:18:26 -0500

    Configure stateValidator middleware

--- React/middlewares-example/src/Root.js
+++ React/middlewares-example/src/Root.js
@@ -3,11 +3,12 @@ import { Provider } from 'react-redux';
 import { createStore, compose, applyMiddleware } from 'redux';
 import reducers from 'reducers';
 import asyncMiddleware from 'middlewares/asyncMiddleware';
+import stateValidator from 'middlewares/stateValidator';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const root = ({ children, initialState = {} }) => {
-    const store = createStore(reducers, initialState, composeEnhancers(applyMiddleware(asyncMiddleware)));
+    const store = createStore(reducers, initialState, composeEnhancers(applyMiddleware(asyncMiddleware, stateValidator)));
     return <Provider store={store}>{children}</Provider>;
 };
 
