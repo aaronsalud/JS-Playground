9a76e8ade12fcb058aaffc7fb0e97027c4135a6d
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-02 06:49:32 -0500

    Setup router

--- React/authentication-example/api/index.js
+++ React/authentication-example/api/index.js
@@ -4,10 +4,13 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
 const app = express();
+const router = require('./router');
 
 // Setup Express middlewares
 app.use(morgan('combined')); // Display request logs in the console
 app.use(bodyParser.json({type: '*/*'})); // Parses incoming requests to JSON
+
+router(app);
 
 const port = process.env.PORT || 3090;
 const server = http.createServer(app);
