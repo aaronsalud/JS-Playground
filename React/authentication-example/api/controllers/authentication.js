eededb0bd9d54b35c8d417842a70658a149cea05
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-02 11:30:41 -0500

    Generate and return JWT after signing up

--- React/authentication-example/api/controllers/authentication.js
+++ React/authentication-example/api/controllers/authentication.js
@@ -1,4 +1,11 @@
+const jwt = require('jwt-simple');
 const User = require('../models/User');
+const config = require('../config');
+
+const generateJWT = (user) => {
+    const timestamp = new Date().getTime();
+    return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+};
 
 exports.signUp = async (req, res, next) => {
     const { email, password } = req.body;
@@ -16,8 +23,7 @@ exports.signUp = async (req, res, next) => {
     const newUser = new User({ email, password });
 
     newUser.save((err) => {
-        console.log('here');
         if (err) return next(err);
-        res.json(user);
+        res.json({ token: generateJWT(newUser) });
     });
 };
