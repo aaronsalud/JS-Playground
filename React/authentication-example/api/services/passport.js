3d9323703b7391622fd88224066c93cb3227a9b3
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-02 11:55:20 -0500

    Add passport strategy for logging in

---
+++ React/authentication-example/api/services/passport.js
@@ -0,0 +1,27 @@
+const passport = require('passport');
+const User = require('../models/User');
+const config = require('../config');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+//Setup options for JWT Strategy
+const jwtOptions = {
+    jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+    secretOrKey: config.secret
+};
+
+//Create JWT Strategy
+
+const jwtLogin = new JwtStrategy(jwtOptions, async (payload, done) => {
+    // Check if the user ID in the payload exists in our DB
+    // If it does, call 'done' and pass the user object
+    // Otherwise, call done without the user object and return false instead
+    const user = await User.findById(payload.sub).exec();
+
+    if (!user) return done(null, false);
+
+    done(null, user);
+});
+
+// Configure this passport strategy
+passport.use(jwtLogin);
