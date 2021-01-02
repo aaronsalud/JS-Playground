92c16ca68a708d865f2f49d9533dfc2ddded5482
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-02 07:17:38 -0500

    Configure DB

--- React/authentication-example/api/index.js
+++ React/authentication-example/api/index.js
@@ -5,6 +5,10 @@ const morgan = require('morgan');
 
 const app = express();
 const router = require('./router');
+const mongoose = require('mongoose');
+
+// DB Setup
+mongoose.connect('mongodb://localhost:auth/auth', { useNewUrlParser: true, useUnifiedTopology: true  });
 
 // Setup Express middlewares
 app.use(morgan('combined')); // Display request logs in the console
