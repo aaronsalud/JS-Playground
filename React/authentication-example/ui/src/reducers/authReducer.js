930254b263f15ec4e7614977879b680418e23935
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-04 14:24:59 -0500

    Add authReducer

---
+++ React/authentication-example/ui/src/reducers/authReducer.js
@@ -0,0 +1,12 @@
+const INITIAL_STATE = {
+    authenticated: '',
+    errorMessage: ''
+};
+const authReducer = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+export default authReducer;
