0a51e843022aebfaf57568d2d66f4154e5a1e42a
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-04 14:52:04 -0500

    Add signup form submit

--- React/authentication-example/ui/src/components/auth/Signup.js
+++ React/authentication-example/ui/src/components/auth/Signup.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { signUp } from '../../actions';
 
 class Signup extends Component {
+
+    onSubmit = (formProps) => {
+        this.props.signUp(formProps);
+    };
+
     render() {
+        const { handleSubmit } = this.props;
         return (
-            <form action="">
+            <form onSubmit={handleSubmit(this.onSubmit)}>
                 <fieldset>
                     <label htmlFor="">Email</label>
-                    <Field name="email" type="text" component="input" autoComplete="none"/>
+                    <Field name="email" type="text" component="input" autoComplete="none" />
                 </fieldset>
                 <fieldset>
                     <label htmlFor="">Password</label>
-                    <Field name="password" type="password" component="input" autoComplete="none"/>
+                    <Field name="password" type="password" component="input" autoComplete="none" />
                 </fieldset>
+                <button>Sign Up!</button>
             </form>
         );
     }
 }
 
-export default reduxForm({ form: 'signup' })(Signup);
+export default compose(
+    connect(null, { signUp }),
+    reduxForm({ form: 'signup' })
+)(Signup);
