6c76f3e3fb0b7dddaf3e71c2077c1340fc0ca403
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-04 14:14:17 -0500

    Create Signup.js

---
+++ React/authentication-example/ui/src/components/auth/Signup.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Signup extends Component {
+    render() {
+        return (
+            <form action="">
+                <fieldset>
+                    <label htmlFor="">Email</label>
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="">Password</label>
+                </fieldset>
+            </form>
+        );
+    }
+}
+
+export default Signup;
