bef509509cffee03c28f14d4cc7faf19d26626c2
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-04 15:50:34 -0500

    Handle AUTH_ERROR action

--- React/authentication-example/ui/src/reducers/authReducer.js
+++ React/authentication-example/ui/src/reducers/authReducer.js
@@ -1,9 +1,15 @@
+import { AUTH_ERROR, AUTH_USER } from '../actions/types';
+
 const INITIAL_STATE = {
     authenticated: '',
     errorMessage: ''
 };
 const authReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
+        case AUTH_USER:
+            return { ...state, authenticated: action.payload };
+        case AUTH_ERROR:
+            return { ...state, errorMessage: action.payload };
         default:
             return state;
     }
