fe8fa335b88e7d48f011dd41225d87ff71d5e246
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-03 19:26:49 -0500

    Add new signin route

--- React/authentication-example/api/router.js
+++ React/authentication-example/api/router.js
@@ -3,10 +3,13 @@ const Authentication = require('./controllers/authentication');
 const passportService = require('./services/passport');
 
 const requireAuth = passport.authenticate('jwt', { session: false });
+const requireSignIn = passport.authenticate('local', { session: false });
 
 module.exports = (app) => {
-    app.get('/', requireAuth, (req, res)=>{
-        res.send({success:true});
+    app.get('/', requireAuth, (req, res) => {
+        res.send({ success: true });
     })
+    app.post('/signin', requireSignIn, Authentication.signIn);
     app.post('/signup', Authentication.signUp);
+
 }
