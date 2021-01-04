62b7cc2a6d0505f34fe74757e2e08c165dfc65d5
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-04 14:10:04 -0500

    Define Welcome route

--- React/authentication-example/ui/src/components/App.js
+++ React/authentication-example/ui/src/components/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './Header';
+import Welcome from './Welcome';
 
 const App = () => {
-    return(
-        <div>App</div>
+    return (
+        <div>
+            <BrowserRouter>
+                <Header />
+                <Route path="/" exact component={Welcome} />
+            </BrowserRouter>
+        </div>
     );
 };
 
