f3d8dafaacb5b72b3e053730822f294b1821e834
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-03 19:19:20 -0500

    Implement local strategy used for logging in

--- React/authentication-example/api/services/passport.js
+++ React/authentication-example/api/services/passport.js
@@ -3,6 +3,24 @@ const User = require('../models/User');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
+
+// Create local strategy
+const localOptions = { usernameField: 'email' }
+const localLogin = new LocalStrategy(localOptions, async (email, password, done) => {
+    // Verify email and password, call done with the user
+    // If it is correct username and password otherwose call done with false
+    const user = await User.findOne({ email }).exec();
+    if (!user) return done(null, false);
+
+    // Compare passwords= is `password` equal to user.password?
+    user.comparePassword(password, (err, isMatch) => {
+        if (err) return done(err);
+        if (!isMatch) return done(null, false)
+
+        return done(null, user);
+    });
+});
 
 //Setup options for JWT Strategy
 const jwtOptions = {
@@ -24,3 +42,4 @@ const jwtLogin = new JwtStrategy(jwtOptions, async (payload, done) => {
 
 // Configure this passport strategy
 passport.use(jwtLogin);
+passport.use(localLogin);
