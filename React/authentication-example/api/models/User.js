edaec0f6b795f824899962ca11574a3d5dbeb44e
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-03 19:18:56 -0500

    Add helper method to user schema to compare passwords

--- React/authentication-example/api/models/User.js
+++ React/authentication-example/api/models/User.js
@@ -23,9 +23,17 @@ userSchema.pre('save', function (next) {
             if (err) return next(err);
             user.password = hash;
             next();
-        })
+        });
     });
 });
+
+userSchema.methods.comparePassword = function (candidatePassword, callback) {
+    bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+        if (err) return callback(err);
+
+        callback(null, isMatch);
+    })
+}
 
 // Create the model class
 const User = mongoose.model('users', userSchema);
