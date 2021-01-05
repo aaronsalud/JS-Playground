f3d6c155d8a969aaacc3d50b1e1b48d8a52ed324
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-05 15:50:05 -0500

    Add signIn action creator method

--- React/authentication-example/ui/src/actions/index.js
+++ React/authentication-example/ui/src/actions/index.js
@@ -20,6 +20,25 @@ export const signUp = ({ email, password }, redirect) => async dispatch => {
     }
 };
 
+export const signIn = ({ email, password }, redirect) => async dispatch => {
+    try {
+        const response = await axios.post('/signin', { email, password });
+        dispatch({
+            type: AUTH_USER,
+            payload: response.data.token
+        });
+        localStorage.setItem('token', response.data.token);
+        redirect();
+    }
+    catch (e) {
+        const errorMessage = e.response.data.error;
+        dispatch({
+            type: AUTH_ERROR,
+            payload: errorMessage
+        })
+    }
+};
+
 export const signOut = () => {
     localStorage.removeItem('token');
     return {
