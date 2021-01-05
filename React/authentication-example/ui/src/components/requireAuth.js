cf85276ad5e7d6cd2fd1723a841a2eebf70f3997
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-05 15:29:07 -0500

    Create requireAuth.js

---
+++ React/authentication-example/ui/src/components/requireAuth.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+const requireAuth = (ChildComponent) => {
+    class ComposedComponent extends Component {
+
+        componentDidMount() {
+            this.navigateAway();
+        }
+
+        componentDidUpdate() {
+            this.navigateAway();
+        }
+
+        navigateAway() {
+            if (!this.props.auth) {
+                this.props.history.push('/');
+            }
+        }
+
+        render() {
+            return <ChildComponent {...this.props}/>
+        }
+    }
+    const mapStateToProps = (state) => {
+        return { auth: state.auth.authenticated }
+    }
+
+    return connect(mapStateToProps)(ComposedComponent);
+};
+
+export default requireAuth;
