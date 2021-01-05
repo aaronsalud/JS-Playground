76895f8522218f65772ec3eb4daa8c65a868261a
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-05 15:44:45 -0500

    Add Signout component

---
+++ React/authentication-example/ui/src/components/auth/Signout.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { signOut } from '../../actions/';
+
+class Signout extends Component {
+
+    componentDidMount() {
+        this.props.signOut();
+    }
+
+    render() {
+        return (
+            <div>Sorry to see you go!</div>
+        );
+    }
+}
+
+export default connect(null, { signOut })(Signout);
