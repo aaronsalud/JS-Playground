8b42f53512bd148e728f8fb0574b178071bbbaf5
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-06 16:06:36 -0500

    Add Card component

---
+++ React/robofriends/src/components/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "tachyons";
+
+const Card = (props) => {
+    return(
+        <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+            <img src="https://robohash.org/test?200x200" alt="robot"/>
+            <div>
+                <h2>Jane Doe</h2>
+                <p>jane.doe@gmail.com</p>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
