4aaa9093982e5baf4feb95353bfaaf086cfea6f6
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-07 14:23:08 -0500

    Update index.js

--- React/robofriends/src/actions/index.js
+++ React/robofriends/src/actions/index.js
@@ -1,8 +1,37 @@
-import { CHANGE_SEARCH_TERM } from './types';
+import axios from 'axios';
+import { CHANGE_SEARCH_TERM, SET_ROBOTS } from './types';
 
-export const setSearchTerm = searchTerm => {
-    return {
+export const setSearchTerm = searchTerm => (dispatch, getState) => {
+    dispatch({
         type: CHANGE_SEARCH_TERM,
         payload: searchTerm
+    });
+
+    const { robots } = getState();
+
+    if (searchTerm) {
+        dispatch(filterRobots(searchTerm, robots));
+    }
+    else {
+        dispatch(fetchRobots());
     }
 };
+
+export const fetchRobots = () => async dispatch => {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+    dispatch(setRobots(response.data));
+}
+
+export const filterRobots = (searchTerm, robots) => {
+    const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+    return setRobots(filteredRobots);
+};
+
+export const setRobots = (robots) => {
+    return {
+        type: SET_ROBOTS,
+        payload: robots
+    }
+}
