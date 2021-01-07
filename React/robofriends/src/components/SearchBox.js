d14bf2807808537869efed410991ef93949656bc
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-07 14:22:42 -0500

    Make searchbox component a controller input by using reference of input from state

--- React/robofriends/src/components/SearchBox.js
+++ React/robofriends/src/components/SearchBox.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { setSearchTerm } from '../actions';
 
-const SearchBox = ({onSearchChange}) => {
+const SearchBox = ({ searchTerm, setSearchTerm }) => {
     return (
         <div className="mb3">
-            <input type="search" placeholder="search robots" onChange={e => onSearchChange(e.target.value)} />
+            <input type="search" placeholder="search robots" value={searchTerm} onChange={e => setSearchTerm(e.target.value)} />
         </div>
     );
 };
 
-export default SearchBox;
+const mapStateToProps = state => {
+    return {
+        searchTerm: state.searchTerm
+    };
+}
+
+export default connect(mapStateToProps, { setSearchTerm })(SearchBox);
