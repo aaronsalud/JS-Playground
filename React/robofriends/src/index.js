f4f1dd47bd3e57f8d1d43c69b7984b37e536186b
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-10 18:11:57 -0500

    Register service worker

--- React/robofriends/src/index.js
+++ React/robofriends/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reduxThunk from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers';
@@ -10,3 +11,9 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducers, composeEnhancers(applyMiddleware(reduxThunk)));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.querySelector('#root'));
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
