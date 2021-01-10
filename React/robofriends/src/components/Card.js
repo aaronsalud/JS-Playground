5b1d68cefec6039cb312ee1e7dc46c1fd2d4caa5
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-10 18:18:48 -0500

    Fix image link param

--- React/robofriends/src/components/Card.js
+++ React/robofriends/src/components/Card.js
@@ -4,7 +4,7 @@ import "tachyons";
 const Card = ({ robot }) => {
     return (
         <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-            <img src={`https://robohash.org/${robot.id}?200x200`} alt="robot" />
+            <img src={`https://robohash.org/${robot.id}?size=200x200`} alt="robot" />
             <div>
                 <h2>{robot.name}</h2>
                 <p>{robot.email}</p>
