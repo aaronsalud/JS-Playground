65b28b5b3ce2b8e6b604e439b937c0971c4aab4d
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-13 19:54:13 -0500

    Create ErrorBoundary.js

---
+++ Next/robofriends-ssr/components/ErrorBoundary.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class ErrorBoundary extends Component {
+    state = { hasError: false };
+
+    componentDidCatch(error, info) {
+        this.setState({ hasError: true });
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return <h1>Error has occured</h1>
+        }
+        else {
+            return this.props.children;
+        }
+    }
+}
+
+export default ErrorBoundary;
