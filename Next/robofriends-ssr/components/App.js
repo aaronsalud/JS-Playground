4c4a59e172d318f3d73cf9e6a57c63a2835ac4e9
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-13 19:54:22 -0500

    Create App.js

---
+++ Next/robofriends-ssr/components/App.js
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import CardList from './CardList';
+import Scroll from './Scroll';
+import SearchBox from './SearchBox';
+import { fetchRobots } from '../actions';
+// import '../styles/App.css';
+
+const App = ({ robots, fetchRobots }) => {
+
+    useEffect(() => {
+        fetchRobots();
+    }, [fetchRobots]);
+
+    return (
+        <div className="tc">
+            <h1 className="f1">RoboFriends</h1>
+            <SearchBox />
+            <Scroll>
+                <CardList robots={robots} />
+            </Scroll>
+        </div>
+    );
+};
+
+const mapStateToProps = state => {
+    return {
+        searchTerm: state.searchTerm,
+        robots: state.robots
+    }
+}
+
+export default connect(mapStateToProps, { fetchRobots })(App);
