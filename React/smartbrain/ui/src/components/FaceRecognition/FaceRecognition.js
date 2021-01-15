324c058882e6c7cdd1bca446eb358b3dcbdc77a2
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-15 16:29:11 -0500

    Add support to displaying multiple face boxes

--- React/smartbrain/ui/src/components/FaceRecognition/FaceRecognition.js
+++ React/smartbrain/ui/src/components/FaceRecognition/FaceRecognition.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './FaceRecognition.css';
 
-const FaceRecognition = ({ imageUrl, box }) => {
+const FaceRecognition = ({ imageUrl, boxes }) => {
+
+  const renderBoxes = () => {
+    return boxes.map(box => {
+      return <div className='bounding-box' key={box.id} style={{ top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol }}></div>
+    });
+  };
+
   return (
     <div className='center ma'>
       <div className='absolute mt2'>
-        <img id='inputimage' alt='' src={imageUrl} width='500px' heigh='auto'/>
-        <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
+        <img id='inputimage' alt='' src={imageUrl} width='500px' heigh='auto' />
+        {renderBoxes()}
       </div>
     </div>
   );
