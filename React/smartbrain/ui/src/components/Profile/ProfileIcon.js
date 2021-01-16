a0abcb107cde0a0f882880409b454cf4afb0392b
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-16 16:20:17 -0500

    Update ProfileIcon.js

--- React/smartbrain/ui/src/components/Profile/ProfileIcon.js
+++ React/smartbrain/ui/src/components/Profile/ProfileIcon.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
-const ProfileIcon = () => {
+const ProfileIcon = (props) => {
     const [dropdownToggle, setDropdownToggle] = useState(false);
 
     return (
@@ -10,9 +10,9 @@ const ProfileIcon = () => {
                 <DropdownToggle caret tag="span" data-toggle="dropdown" aria-expanded={dropdownToggle}>
                     <img src="http://tachyons.io/img/logo.jpg" className="br-100 ba h3 w3 dib" alt="avatar" />
                 </DropdownToggle>
-                <DropdownMenu style={{marginTop: '20px', backgroundColor: 'rgba(255 255 255 0.5)'}}>
-                    <DropdownItem>View Profile</DropdownItem>
-                    <DropdownItem>Signout</DropdownItem>
+                <DropdownMenu right style={{ marginTop: '20px', backgroundColor: 'rgba(255 255 255 0.5)' }}>
+                    <DropdownItem onClick={() => props.onRouteChange('profile')}>View Profile</DropdownItem>
+                    <DropdownItem onClick={() => props.onRouteChange('signout')}>Signout</DropdownItem>
                 </DropdownMenu>
             </Dropdown>
         </div>
