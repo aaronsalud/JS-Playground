2a302f7cdd50562d9981271d5f1714610bb2e5aa
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-18 13:52:00 -0500

    Handle sessions with redis

--- React/smartbrain/api/controllers/signin.js
+++ React/smartbrain/api/controllers/signin.js
@@ -27,8 +27,15 @@ const handleSignin = (db, bcrypt, req, res) => {
     .catch(err => Promise.reject('wrong credentials'))
 }
 
-const getAuthTokenId = () => {
-  console.log('auth ok');
+const getAuthTokenId = (req, res) => {
+  const { authorization } = req.headers;
+
+  return redisClient.get(authorization, (err, reply) => {
+    if (err || !reply) {
+      return res.status(400).json('Unauthorized');
+    }
+    return res.json({ id: reply });
+  });
 }
 
 const signToken = email => {
@@ -36,15 +43,22 @@ const signToken = email => {
   return jwt.sign(jwtPayload, process.env.JWT_SECRET_KEY, { expiresIn: '2 days' });
 }
 
+const setToken = (token, id) => {
+  return Promise.resolve(redisClient.set(token, id));
+}
+
 const createSession = user => {
   const { email, id } = user;
   const token = signToken(email);
-  return Promise.resolve({ success: 'true', userId: id, token });
+
+  return setToken(token, id)
+    .then(() => ({ success: true, userId: id, token }))
+    .catch((err) => console.log(err));
 }
 
 const signinAuthentication = (db, bcrypt) => (req, res) => {
   const { authorization } = req.headers;
-  return authorization ? getAuthTokenId() :
+  return authorization ? getAuthTokenId(req, res) :
     handleSignin(db, bcrypt, req, res)
       .then(data => data.id && data.email ? createSession(data) : Promise.reject(data))
       .then(session => res.json(session))
