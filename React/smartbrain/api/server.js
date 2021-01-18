9df1e65782e75805c2f82380386f5bb13464e628
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-18 14:22:53 -0500

    Apply auth middlewares to private routes

--- React/smartbrain/api/server.js
+++ React/smartbrain/api/server.js
@@ -9,6 +9,7 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 const morgan = require('morgan');
+const { requireAuth } = require('./controllers/authorization');
 require('dotenv').config();
 
 const db = knex({
@@ -26,9 +27,9 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => { res.send(db.users) })
 app.post('/signin', signin.signinAuthentication(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
-app.put('/image', (req, res) => { image.handleImage(req, res, db) })
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
+app.get('/profile/:id', requireAuth,(req, res) => { profile.handleProfileGet(req, res, db) })
+app.put('/image', requireAuth, (req, res) => { image.handleImage(req, res, db) })
+app.post('/imageurl', requireAuth,(req, res) => { image.handleApiCall(req, res) })
 
 app.listen(5000, () => {
   console.log('app is running on port 5000');
