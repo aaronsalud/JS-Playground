0907ca3d8e4d7713700d01c74d2cba413fee5a5a
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-20 15:14:07 -0500

    Render rank component with data

--- React/smartbrain/ui/src/components/Dashboard/Dashboard.js
+++ React/smartbrain/ui/src/components/Dashboard/Dashboard.js
@@ -1,21 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Logo from './Logo/Logo';
 import Rank from './Rank';
 import ImageLinkForm from './ImageLinkForm/ImageLinkForm';
 import FaceRecognition from './FaceRecognition/FaceRecognition';
 
 const Dashboard = (props) => {
+    const { name, entries } = props.user;
 
     return (
         <div>
             <Logo />
-            <Rank
-                // name={this.state.user.name}
-                // entries={this.state.user.entries}
-            />
+            <Rank name={name} entries={entries} />
             <ImageLinkForm
-                // onInputChange={this.onInputChange}
-                // onButtonSubmit={this.onButtonSubmit}
+            // onInputChange={this.onInputChange}
+            // onButtonSubmit={this.onButtonSubmit}
             />
             {/* <FaceRecognition 
             boxes={boxes} imageUrl={imageUrl} 
@@ -25,4 +24,10 @@ const Dashboard = (props) => {
 
 };
 
-export default Dashboard;
+const mapStateToProps = state => {
+    return {
+        user: state.user
+    }
+}
+
+export default connect(mapStateToProps)(Dashboard);
