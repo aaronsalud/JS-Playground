c0f868e57dc9e1d3a6a021587ebcb3ff855ea4d9
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-20 15:02:03 -0500

    Add action method to set user data if token exists

--- React/smartbrain/ui/src/actions/index.js
+++ React/smartbrain/ui/src/actions/index.js
@@ -3,20 +3,23 @@ import { SET_USER } from './types';
 import setAuthHeader from '../utils/setAuthHeader';
 
 export const fetchUser = async id => {
-    try{
+    try {
         const { data } = await axios.get(`/profile/${id}`);
         if (data) {
             return {
                 type: SET_USER,
                 payload: data
-            }; 
+            };
         }
     }
-    catch(e){
+    catch (e) {
         console.log(e);
     }
-   
 };
+
+export const setUser = (id) => async dispatch => {
+    dispatch(await fetchUser(id));
+}
 
 export const signIn = (email, password) => async dispatch => {
     try {
@@ -24,7 +27,7 @@ export const signIn = (email, password) => async dispatch => {
         window.sessionStorage.setItem('token', data.token);
         setAuthHeader(data.token);
         dispatch(await fetchUser(data.userId));
-          // this.onRouteChange('home');
+        // this.onRouteChange('home');
     }
     catch (e) {
         console.log(e);
