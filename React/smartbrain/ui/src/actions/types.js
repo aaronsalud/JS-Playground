2cc9d6c82b2e1d8ee80bdc5e3be69c1bbacdfa1f
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-21 17:54:24 -0500

    Add SET_IMAGE_BOXES action type

--- React/smartbrain/ui/src/actions/types.js
+++ React/smartbrain/ui/src/actions/types.js
@@ -1,3 +1,4 @@
 export const SET_USER = 'SET_USER';
 export const SET_IMAGE_URL = 'SET_IMAGE_URL';
+export const SET_IMAGE_BOXES = 'SET_IMAGE_BOXES';
 export const UPDATE_USER_ENTRIES = 'UPDATE_USER_ENTRIES';
