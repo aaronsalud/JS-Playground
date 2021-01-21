be5c41d60f765e6b199f1081096889111a5a87a9
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-21 17:23:55 -0500

    Add image input state

--- React/smartbrain/ui/src/components/Dashboard/ImageLinkForm/ImageLinkForm.js
+++ React/smartbrain/ui/src/components/Dashboard/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ImageLinkForm.css';
 
-const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
+const ImageLinkForm = (props) => {
+
+  const [image, setImage] = useState('');
+
   return (
     <div>
       <p className='f3'>
@@ -9,10 +12,10 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
       </p>
       <div className='center'>
         <div className='form center pa4 br3 shadow-5'>
-          <input className='f4 pa2 w-70 center' type='tex' onChange={onInputChange}/>
+          <input className='f4 pa2 w-70 center' type='tex' value={image} onChange={(e) => setImage(e.target.value)} />
           <button
             className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
-            onClick={onButtonSubmit}
+            onClick={onSubmit}
           >Detect</button>
         </div>
       </div>
