f1e3d67f5463115d71583e14ca08d8e1a0a427f1
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-21 17:47:56 -0500

    Add reducer statement to update user entries

--- React/smartbrain/ui/src/reducers/userReducer.js
+++ React/smartbrain/ui/src/reducers/userReducer.js
@@ -1,9 +1,11 @@
-import { SET_USER } from '../actions/types';
+import { SET_USER, UPDATE_USER_ENTRIES } from '../actions/types';
 
 const userReducer = (state = null, action) => {
     switch (action.type) {
         case SET_USER:
             return action.payload;
+        case UPDATE_USER_ENTRIES:
+            return { ...state, entries: action.payload };
         default:
             return state;
     }
