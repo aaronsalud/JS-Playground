cb385952df013b86cb5c4ce211219b613083301b
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-23 13:16:03 -0500

    Update types.js

--- React/smartbrain/ui/src/actions/types.js
+++ React/smartbrain/ui/src/actions/types.js
@@ -2,3 +2,4 @@ export const SET_USER = 'SET_USER';
 export const SET_IMAGE_URL = 'SET_IMAGE_URL';
 export const SET_IMAGE_BOXES = 'SET_IMAGE_BOXES';
 export const UPDATE_USER_ENTRIES = 'UPDATE_USER_ENTRIES';
+export const UNSET_USER = 'UNSET_USER';
