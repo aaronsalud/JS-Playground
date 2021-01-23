e9614152af65d3a6f0837f6404b4ff7eaf4f8328
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-23 13:25:05 -0500

    Cleanup App component

--- React/smartbrain/ui/src/App.js
+++ React/smartbrain/ui/src/App.js
@@ -17,39 +17,18 @@ import setAuthHeader from './utils/setAuthHeader';
 import history from './history';
 import './App.css';
 
-const particlesOptions = {
-  //customize this to your liking
-  particles: {
-    number: {
-      value: 30,
-      density: {
-        enable: true,
-        value_area: 800
+class App extends Component {
+
+  state = {
+    particles: {
+      number: {
+        value: 30,
+        density: {
+          enable: true,
+          value_area: 800
+        }
       }
     }
-  }
-}
-
-const initialState = {
-  input: '',
-  imageUrl: '',
-  boxes: [],
-  route: 'signin',
-  isSignedIn: false,
-  isProfileModalOpen: false,
-  user: {
-    id: '',
-    name: '',
-    email: '',
-    entries: 0,
-    joined: ''
-  }
-}
-
-class App extends Component {
-  constructor() {
-    super();
-    this.state = initialState;
   }
 
   componentDidMount() {
@@ -62,73 +41,12 @@ class App extends Component {
     }
   }
 
-  calculateFaceLocations = (data) => {
-    return data.outputs[0].data.regions.map(face => {
-      const clarifaiFace = face.region_info.bounding_box;
-      const image = document.getElementById('inputimage');
-      const width = Number(image.width);
-      const height = Number(image.height);
-      return {
-        id: face.id,
-        leftCol: clarifaiFace.left_col * width,
-        topRow: clarifaiFace.top_row * height,
-        rightCol: width - (clarifaiFace.right_col * width),
-        bottomRow: height - (clarifaiFace.bottom_row * height)
-      }
-    });
-  }
-
-  displayFaceBoxes = (boxes) => {
-    this.setState({ boxes });
-  }
-
-  onButtonSubmit = () => {
-    this.setState({ imageUrl: this.state.input });
-    const token = window.sessionStorage.getItem('token');
-    fetch('/imageurl', {
-      method: 'post',
-      headers: { 'Content-Type': 'application/json', 'Authorization': token },
-      body: JSON.stringify({
-        input: this.state.input
-      })
-    })
-      .then(response => response.json())
-      .then(response => {
-        if (response) {
-          fetch('/image', {
-            method: 'put',
-            headers: { 'Content-Type': 'application/json', 'Authorization': token },
-            body: JSON.stringify({
-              id: this.state.user.id
-            })
-          })
-            .then(response => response.json())
-            .then(count => {
-              this.setState(Object.assign(this.state.user, { entries: count }))
-            })
-            .catch(console.log)
-
-        }
-        this.displayFaceBoxes(this.calculateFaceLocations(response));
-      })
-      .catch(err => console.log(err));
-  }
-
-  onRouteChange = (route) => {
-    if (route === 'signout') {
-      return this.setState(initialState)
-    } else if (route === 'home') {
-      this.setState({ isSignedIn: true })
-    }
-    this.setState({ route: route });
-  }
-
   toggleProfileModal = () => {
     this.setState({ isProfileModalOpen: !this.state.isProfileModalOpen });
   }
 
   render() {
-    const { isSignedIn, imageUrl, route, boxes, isProfileModalOpen, user } = this.state;
+    const { particlesOptions } = this.state;
     return (
       <div className="App">
 
