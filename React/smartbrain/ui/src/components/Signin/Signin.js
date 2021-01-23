1fe9d9130cce093cbd845cee134043b766f5c98f
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-23 12:24:18 -0500

    Cleanup Signin component

--- React/smartbrain/ui/src/components/Signin/Signin.js
+++ React/smartbrain/ui/src/components/Signin/Signin.js
@@ -1,67 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { signIn } from '../../actions';
 
-class Signin extends React.Component {
+const Signin = ({ signIn }) => {
 
-  state = { email: '', password: '' };
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      password: ''
-    }
-  }
-
-  onSubmitSignIn = (e) => {
+  const onSubmitSignIn = (e) => {
     e.preventDefault();
-    const { email, password } = this.state;
-    this.props.signIn(email, password);
+    signIn(email, password);
   }
 
-  render() {
-    const { onRouteChange } = this.props;
-    return (
-      <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-        <main className="pa4 black-80">
-          <div className="measure">
-            <form onSubmit={this.onSubmitSignIn}>
-              <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                <legend className="f1 fw6 ph0 mh0">Sign In</legend>
-                <div className="mt3">
-                  <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                  <input
-                    className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 hover-black"
-                    type="email"
-                    name="email-address"
-                    id="email-address"
-                    value={this.state.email}
-                    onChange={(e) => this.setState({ email: e.target.value })}
-                  />
-                </div>
-                <div className="mv3">
-                  <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                  <input
-                    className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 hover-black"
-                    type="password"
-                    name="password"
-                    id="password"
-                    value={this.state.password}
-                    onChange={(e) => this.setState({ password: e.target.value })}
-                  />
-                </div>
-              </fieldset>
-              <button className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit">Sign In</button>
-            </form>
-            <div className="lh-copy mt3">
-              <p onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
-            </div>
+  return (
+    <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+      <main className="pa4 black-80">
+        <div className="measure">
+          <form onSubmit={onSubmitSignIn}>
+            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+              <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+              <div className="mt3">
+                <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+                <input
+                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 hover-black"
+                  type="email"
+                  name="email-address"
+                  id="email-address"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="mv3">
+                <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+                <input
+                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 hover-black"
+                  type="password"
+                  name="password"
+                  id="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+            </fieldset>
+            <button className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit">Sign In</button>
+          </form>
+          <div className="lh-copy mt3">
+            <p className="f6 link dim black db pointer">Register</p>
           </div>
-        </main>
-      </article>
-    );
-  }
+        </div>
+      </main>
+    </article>
+  );
 }
 
 export default connect(null, { signIn })(Signin);
