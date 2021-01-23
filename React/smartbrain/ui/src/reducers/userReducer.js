66f31b896ba97aebb580b18cf6a4016ccdd22232
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-23 13:16:08 -0500

    Update userReducer.js

--- React/smartbrain/ui/src/reducers/userReducer.js
+++ React/smartbrain/ui/src/reducers/userReducer.js
@@ -1,4 +1,4 @@
-import { SET_USER, UPDATE_USER_ENTRIES } from '../actions/types';
+import { SET_USER, UNSET_USER, UPDATE_USER_ENTRIES } from '../actions/types';
 
 const userReducer = (state = null, action) => {
     switch (action.type) {
@@ -6,6 +6,8 @@ const userReducer = (state = null, action) => {
             return action.payload;
         case UPDATE_USER_ENTRIES:
             return { ...state, entries: action.payload };
+        case UNSET_USER:
+            return null;
         default:
             return state;
     }
