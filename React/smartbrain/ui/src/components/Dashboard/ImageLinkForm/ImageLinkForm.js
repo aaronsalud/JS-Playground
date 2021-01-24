8edcae4d89b807a2efca1c44dff1089c729b342a
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-24 17:47:29 -0500

    Wrap input in a form

--- React/smartbrain/ui/src/components/Dashboard/ImageLinkForm/ImageLinkForm.js
+++ React/smartbrain/ui/src/components/Dashboard/ImageLinkForm/ImageLinkForm.js
@@ -16,13 +16,12 @@ const ImageLinkForm = ({ setImageUrl, getImageRecognitionResults }) => {
         {'This Magic Brain will detect faces in your pictures. Git it a try.'}
       </p>
       <div className='center'>
-        <div className='form center pa4 br3 shadow-5'>
-          <input className='f4 pa2 w-70 center' type='tex' value={image} onChange={(e) => setImage(e.target.value)} />
-          <button
-            className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
-            onClick={onSubmit}
-          >Detect</button>
-        </div>
+        <form onSubmit={onSubmit}>
+          <div className='form center pa4 br3 shadow-5'>
+            <input className='f4 pa2 w-70 center' type='tex' value={image} onChange={(e) => setImage(e.target.value)} />
+            <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple' type="submit">Detect</button>
+          </div>
+        </form>
       </div>
     </div>
   );
