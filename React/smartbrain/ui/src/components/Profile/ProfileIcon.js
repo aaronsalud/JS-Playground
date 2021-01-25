d6dd27774a368128b677e38138645340fc343f2f
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-25 13:51:05 -0500

    Set userImage in ProfileIcon component

--- React/smartbrain/ui/src/components/Profile/ProfileIcon.js
+++ React/smartbrain/ui/src/components/Profile/ProfileIcon.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
-const ProfileIcon = ({ onSignOut, toggleProfileModal }) => {
+const ProfileIcon = ({ onSignOut, toggleProfileModal, userImage }) => {
     const [dropdownToggle, setDropdownToggle] = useState(false);
 
     return (
         <div className="pa4 tc">
             <Dropdown isOpen={dropdownToggle} toggle={() => setDropdownToggle(!dropdownToggle)}>
                 <DropdownToggle caret tag="span" data-toggle="dropdown" aria-expanded={dropdownToggle}>
-                    <img src="http://tachyons.io/img/logo.jpg" className="br-100 ba h3 w3 dib" alt="avatar" />
+                    <img src={userImage} className="br-100 ba h3 w3 dib" alt="avatar" />
                 </DropdownToggle>
                 <DropdownMenu right style={{ marginTop: '20px', backgroundColor: 'rgba(255 255 255 0.5)' }}>
                     <DropdownItem onClick={toggleProfileModal}>View Profile</DropdownItem>
