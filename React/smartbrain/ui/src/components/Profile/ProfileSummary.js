7b0449b21f7fc096a103b6b3d2107988f773106f
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-25 13:55:14 -0500

    Display profile image based on user data

--- React/smartbrain/ui/src/components/Profile/ProfileSummary.js
+++ React/smartbrain/ui/src/components/Profile/ProfileSummary.js
@@ -5,7 +5,7 @@ const ProfileSummary = ({user}) => {
         <div className="profile">
             <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
                 <main className="pa4 black-80">
-                <img src="http://tachyons.io/img/logo.jpg" className="br-100 ba h3 w3 dib" alt="avatar" />
+                <img src={user.profile_image}className="br-100 ba h3 w3 dib" alt="avatar" />
                 <h1>{user.name}</h1>
                 <h4>{`Images Submitted: ${user.entries}`}</h4>
                 <p>{`Member Since: ${new Date(user.joined).toLocaleDateString()}`}</p>
