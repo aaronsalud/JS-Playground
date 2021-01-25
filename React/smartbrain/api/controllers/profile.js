ca35d652c8f341d275c593c3feebfed2ac219fa1
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-25 13:45:25 -0500

    Add functionality to generate gravatar image based on md5 hash of user email

--- React/smartbrain/api/controllers/profile.js
+++ React/smartbrain/api/controllers/profile.js
@@ -1,9 +1,17 @@
+const cryptoJS = require('crypto-js');
+
+const generateGravatarImage = email => {
+  const md5Hash = cryptoJS.MD5(email).toString();
+  return `https://www.gravatar.com/avatar/${md5Hash}`;
+}
+
 const handleProfileGet = (req, res, db) => {
   const { id } = req.params;
-  db.select('*').from('users').where({id})
+  db.select('*').from('users').where({ id })
     .then(user => {
       if (user.length) {
-        res.json(user[0])
+        let userData = { ...user[0], profile_image: generateGravatarImage(user[0].email) };
+        res.json(userData)
       } else {
         res.status(400).json('Not found')
       }
