3e26bec52b3bcd2886038fb35686c2b7fcbe612d
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-26 12:36:59 -0500

    Cleanup

--- React/smartbrain/api/controllers/signin.js
+++ React/smartbrain/api/controllers/signin.js
@@ -6,25 +6,12 @@ require('dotenv').config();
 //Setup Redis
 const redisClient = redis.createClient(process.env.REDIS_URI);
 
-const getAuthTokenId = (req, res) => {
-  const { authorization } = req.headers;
-
-  return redisClient.get(authorization, (err, reply) => {
-    if (err || !reply) {
-      return res.status(400).json('Unauthorized');
-    }
-    return res.json({ id: reply });
-  });
-}
-
 const signToken = id => {
   const jwtPayload = { sub: id };
   return jwt.sign(jwtPayload, process.env.JWT_SECRET_KEY, { expiresIn: '2 days' });
 }
 
-const setToken = async (token, id) => {
-  return await redisClient.set(token, id);
-}
+const setToken = async (token, id) => await redisClient.set(token, id);
 
 const createSession = async user => {
   const { id } = user;
@@ -32,9 +19,9 @@ const createSession = async user => {
 
   try {
     const result = setToken(token, id);
-    if (!result) {
-      return { error: 'An error has occured when signing in - Please contact your admin' };
-    }
+
+    if (!result) return { error: 'An error has occured when signing in - Please contact your admin' };
+
     return { success: true, userId: id, token };
   }
   catch (e) {
@@ -45,20 +32,15 @@ const createSession = async user => {
 const handleSignin = async (db, req, res) => {
   const { email, password } = req.body;
 
-  if (!email || !password) {
-    return res.status(404).json({ error: "Invalid auth credentials" });
-  }
+  if (!email || !password) return res.status(404).json({ error: "Invalid auth credentials" });
 
   try {
     const users = await db.select('*').from('users').where('email', '=', email);
     const isValid = bcrypt.compareSync(password, users[0].password);
 
-    if (!isValid) {
-      return res.status(404).json({ error: "Invalid auth credentials" });
-    }
+    if (!isValid) return res.status(404).json({ error: "Invalid auth credentials" });
 
     const result = await createSession(users[0]);
-
     return res.status(200).json(result);
   }
   catch (e) {
