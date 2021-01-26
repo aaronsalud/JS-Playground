2a489efc597a1bcd4c111cdb644e929899df95e3
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-26 11:34:10 -0500

    Update deploy_schemas.sql

--- React/smartbrain/api/postgres/deploy_schemas.sql
+++ React/smartbrain/api/postgres/deploy_schemas.sql
@@ -1,7 +1,6 @@
 -- Deploy fresh database tables:
 \i '/docker-entrypoint-initdb.d/tables/users.sql'
 \i '/docker-entrypoint-initdb.d/tables/images.sql'
-\i '/docker-entrypoint-initdb.d/tables/login.sql'
 
 -- For testing purposes only. This file will add dummy data
 \i '/docker-entrypoint-initdb.d/seed/seed.sql'
