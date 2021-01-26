85fef45fdf2f85d3921e073857be53c3bfbcbb9d
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-26 12:29:57 -0500

    Update authorization.js

--- React/smartbrain/api/controllers/authorization.js
+++ React/smartbrain/api/controllers/authorization.js
@@ -3,11 +3,11 @@ const redisClient = require('./signin').redisClient;
 const requireAuth = (req, res, next) => {
     const { authorization } = req.headers;
     if (!authorization) {
-        return res.status(401).json('Unathorized');
+        return res.status(401).json('Unauthorized');
     }
     return redisClient.get(authorization, (err, reply) => {
         if (err || !reply) {
-            return res.status(401).json('Unathorized');
+            return res.status(401).json('Unauthorized');
         }
         return next();
     });
