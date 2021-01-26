eb7811e6160c8a20c6a131d43c105226f81be33e
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-26 12:24:47 -0500

    Update server.js

--- React/smartbrain/api/server.js
+++ React/smartbrain/api/server.js
@@ -25,7 +25,7 @@ app.use(cors())
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => { res.send(db.users) })
-app.post('/signin', signin.signinAuthentication(db, bcrypt))
+app.post('/signin', (req, res) => signin.signinAuthentication(db, req, res));
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', requireAuth, (req, res) => { profile.handleProfileGet(req, res, db) })
 app.put('/image', requireAuth, (req, res) => { image.handleImage(req, res, db) })
