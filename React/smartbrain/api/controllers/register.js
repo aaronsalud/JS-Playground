4cb0ee1a184083c8da6b44af8e2a34dddcb6abc1
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-27 14:00:37 -0500

    Rewrite registration flow

--- React/smartbrain/api/controllers/register.js
+++ React/smartbrain/api/controllers/register.js
@@ -1,36 +1,30 @@
-const handleRegister = (req, res, db, bcrypt) => {
+const bcrypt = require('bcrypt-nodejs');
+
+const handleRegister = async (req, res, db) => {
   const { email, name, password } = req.body;
   if (!email || !name || !password) {
-    return res.status(400).json('incorrect form submission');
+    return res.status(400).json('All input fields are required');
   }
+
+  const userExist = await db.select('*').from('users').where('email', '=', email);
+
+  if (userExist.length === 1) {
+    return res.status(400).json({ error: 'User already exists' });
+  }
+
   const hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
-      trx.insert({
-        hash: hash,
-        email: email
-      })
-      .into('login')
-      .returning('email')
-      .then(loginEmail => {
-        return trx('users')
-          .returning('*')
-          .insert({
-            email: loginEmail[0],
-            name: name,
-            joined: new Date()
-          })
-          .then(user => {
-            res.json(user[0]);
-          })
-      })
-      .then(trx.commit)
-      .catch(trx.rollback)
-    })
-    .catch(err => res.status(400).json('unable to register'))
+
+  try {
+    const user = await db('users').insert({ email, name, password: hash, joined: new Date() }).returning(['name', 'email', 'joined']);
+    return res.status(404).json(user);
+  }
+  catch (e) {
+    return res.status(400).json({ error: 'Failed to create and account' })
+  }
 }
 
 module.exports = {
-  handleRegister: handleRegister
+  handleRegister
 };
 
 
