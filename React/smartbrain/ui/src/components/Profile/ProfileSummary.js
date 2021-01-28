d7aff9c02c95d17c667532f341a66a3399c31ac3
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-28 13:20:55 -0500

    Convert ProfileSummary to a container component

--- React/smartbrain/ui/src/components/Profile/ProfileSummary.js
+++ React/smartbrain/ui/src/components/Profile/ProfileSummary.js
@@ -1,19 +1,31 @@
 import React from 'react';
+import { Card } from 'reactstrap';
+import { connect } from 'react-redux';
 
-const ProfileSummary = ({user}) => {
+const ProfileSummary = ({ user }) => {
+
+    if (!user) {
+        return <div>Loading...</div>
+    }
+
     return (
         <div className="profile">
-            <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+            <Card className="br3 ba b--black-10 mv4 w-100 w-50-m mw8 w-25-l shadow-5 center">
                 <main className="pa4 black-80">
-                <img src={user.profile_image}className="br-100 ba h3 w3 dib" alt="avatar" />
-                <h1>{user.name}</h1>
-                <h4>{`Images Submitted: ${user.entries}`}</h4>
-                <p>{`Member Since: ${new Date(user.joined).toLocaleDateString()}`}</p>
-                <hr/>    
+                    <img src={user.profile_image} className="br-100 ba h3 w3 dib" alt="avatar" />
+                    <h1>{user.name}</h1>
+                    <h4>{`Images Submitted: ${user.entries}`}</h4>
+                    <p>{`Member Since: ${new Date(user.joined).toLocaleDateString()}`}</p>
                 </main>
-            </article>
+            </Card>
         </div>
     );
 }
 
-export default ProfileSummary;
+const mapStateToProps = state => {
+    return {
+        user: state.user
+    }
+};
+
+export default connect(mapStateToProps)(ProfileSummary);
