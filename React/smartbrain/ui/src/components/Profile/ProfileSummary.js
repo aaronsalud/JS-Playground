bdfc59e59d682fa03f942a12f93600a4dbae5645
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-28 13:42:29 -0500

    Update ProfileSummary.js

--- React/smartbrain/ui/src/components/Profile/ProfileSummary.js
+++ React/smartbrain/ui/src/components/Profile/ProfileSummary.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Logo from '../Logo/Logo';
 import { Card } from 'reactstrap';
 import { connect } from 'react-redux';
 
@@ -9,13 +10,18 @@ const ProfileSummary = ({ user }) => {
     }
 
     return (
-        <div className="profile">
-            <Card className="br3 ba b--black-10 mv4 w-100 w-50-m mw8 w-25-l shadow-5 center">
-                <main className="pa4 black-80">
-                    <img src={user.profile_image} className="br-100 ba h3 w3 dib" alt="avatar" />
-                    <h1>{user.name}</h1>
-                    <h4>{`Images Submitted: ${user.entries}`}</h4>
-                    <p>{`Member Since: ${new Date(user.joined).toLocaleDateString()}`}</p>
+        <div className="profile flex">
+            <Logo />
+            <Card className="br3 ba b--black-10 w-80 mw8 shadow-5 center mr-4">
+                <main className="pa4 black-80 flex">
+                    <div className="w-50">
+                        <img src={user.profile_image} className="br-100 ba h3 w3 dib" alt="avatar" />
+                        <h1>{user.name}</h1>
+                    </div>
+                    <div className="w-50 py-5">
+                        <h4>{`Images Submitted: ${user.entries}`}</h4>
+                        <p>{`Member Since: ${new Date(user.joined).toLocaleDateString()}`}</p>
+                    </div>
                 </main>
             </Card>
         </div>
