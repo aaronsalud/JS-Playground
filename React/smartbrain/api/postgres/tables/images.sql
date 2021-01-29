9c849a6fa9ed750d220f68d21db187abc78a4d37
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-29 13:03:00 -0500

    Remove field restriction

--- React/smartbrain/api/postgres/tables/images.sql
+++ React/smartbrain/api/postgres/tables/images.sql
@@ -3,7 +3,7 @@ BEGIN TRANSACTION;
 CREATE TABLE images (
     id serial PRIMARY KEY,
     url TEXT,
-    analysis_results json NOT NULL,
+    analysis_results json,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
