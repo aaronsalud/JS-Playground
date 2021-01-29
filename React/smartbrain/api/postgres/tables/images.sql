1792927e045a531f66f8e688a5af6f96708f8312
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-29 12:44:10 -0500

    Update images.sql

--- React/smartbrain/api/postgres/tables/images.sql
+++ React/smartbrain/api/postgres/tables/images.sql
@@ -3,6 +3,7 @@ BEGIN TRANSACTION;
 CREATE TABLE images (
     id serial PRIMARY KEY,
     url TEXT,
+    analysis_results json NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
