46fb1c73b9bb3bdc3cff00dc610165d743125978
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-29 13:15:07 -0500

    Rewrite queries

--- React/smartbrain/api/controllers/register.js
+++ React/smartbrain/api/controllers/register.js
@@ -6,14 +6,14 @@ const handleRegister = async (req, res, db) => {
 
   if (!email || !name || !password) return res.status(400).json('All input fields are required');
 
-  const users = await db.select('*').from('users').where('email', '=', email);
+  const userExists = await db('users').first().where('email', '=', email);
 
-  if (users && users.length) return res.status(400).json({ error: 'User already exists' });
+  if (userExists) return res.status(400).json({ error: 'User already exists' });
 
   try {
     const hash = bcrypt.hashSync(password);
-    const users = await db('users').insert({ email, name, password: hash, joined: new Date() }).returning(['id']);
-    const result = await createSession(users[0]);
+    const userId = await db('users').first().insert({ email, name, password: hash, joined: new Date() }).returning('id');
+    const result = await createSession(userId);
     return res.json(result);
   }
   catch (e) { return res.status(400).json({ error: 'Failed to create and account - Please contact your admin' }) }
