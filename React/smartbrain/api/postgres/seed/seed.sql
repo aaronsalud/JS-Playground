11bba00d15387ac8357c06e41950e3fbd1b20f78
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-29 11:52:25 -0500

    Update seed.sql

--- React/smartbrain/api/postgres/seed/seed.sql
+++ React/smartbrain/api/postgres/seed/seed.sql
@@ -1,4 +1,4 @@
 
 -- Seed data with a fake user for testing
 
-insert into users (name, email, password,entries, joined) values ('test', 'test@example.com', '$2a$10$SN8IexUztcPNfUolx9zRde1RlSep/cHouG7nn.WlqDpfnAEOFtVuq',5, '2021-01-01');
+insert into users (name, email, password,entries, joined) values ('test', 'test@example.com', '$2a$10$SN8IexUztcPNfUolx9zRde1RlSep/cHouG7nn.WlqDpfnAEOFtVuq',0, '2021-01-01');
