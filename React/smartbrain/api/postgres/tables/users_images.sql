70807cddde03917373de857c0ddc033ad7c540cb
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-29 13:02:50 -0500

    Delete bridge table entry if image or user is deleted

--- React/smartbrain/api/postgres/tables/users_images.sql
+++ React/smartbrain/api/postgres/tables/users_images.sql
@@ -3,10 +3,10 @@ BEGIN TRANSACTION;
 CREATE TABLE users_images (
     user_id INT NOT NULL,
     FOREIGN KEY (user_id)
-      REFERENCES users (id),
+      REFERENCES users (id) ON DELETE CASCADE,
     image_id INT NOT NULL,
     FOREIGN KEY (image_id)
-      REFERENCES images (id)
+      REFERENCES images (id) ON DELETE CASCADE
 );
 
 COMMIT;
