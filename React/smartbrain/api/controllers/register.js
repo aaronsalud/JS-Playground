ea973ff6a146b5355b049f31e2668abbf0ebcc19
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-30 12:34:46 -0500

    Add better error handling in account creation

--- React/smartbrain/api/controllers/register.js
+++ React/smartbrain/api/controllers/register.js
@@ -4,21 +4,26 @@ const { createSession } = require('./authorization');
 const handleRegister = async (req, res, db) => {
   try {
     const { email, name, password } = req.body;
+    const errorMessage = {
+      inputFields: 'All input fields are required',
+      existingUser: 'User already exists',
+      accountCreation: 'Failed to create an account - Please contact your admin'
+    };
 
-    if (!email || !name || !password) throw 'All input fields are required';
+    if (!email || !name || !password) throw errorMessage.inputFields;
 
     const userExists = await db('users').first().where('email', email);
-
-    if (userExists) throw 'User already exists';
+    if (userExists) throw errorMessage.existingUser;
 
     const userId = (await db('users').insert({ email, name, password: bcrypt.hashSync(password), joined: new Date() }).returning('id')).toString();
-
-    if (!userId) throw 'Failed to create and account - Please contact your admin';
+    if (!userId) throw errorMessage.accountCreation;
 
     const session = createSession(userId);
+    if (!session) throw errorMessage.accountCreation;
+
     return res.json(session);
   }
-  catch (e) { return res.status(400).json({ error: e }) }
+  catch (error) { return res.status(400).json({ error }); }
 }
 
 module.exports = {
