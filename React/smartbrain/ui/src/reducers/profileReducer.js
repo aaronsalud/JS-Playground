c035b2ff971d2362bd6ea648b2646a491d852235
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-31 00:17:02 -0500

    Add reducer statement to set posted images

--- React/smartbrain/ui/src/reducers/profileReducer.js
+++ React/smartbrain/ui/src/reducers/profileReducer.js
@@ -1,9 +1,12 @@
+import { SET_POSTED_IMAGES } from '../actions/types';
 const initialState = {
     images: []
 };
 
 const profileReducer = (state = initialState, action) => {
     switch (action.type) {
+        case SET_POSTED_IMAGES:
+            return action.payload;
         default:
             return state;
     }
