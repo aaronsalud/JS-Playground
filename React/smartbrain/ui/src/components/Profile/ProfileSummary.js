7c9e54cf46d48fec65b1531a618118d443b57579
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-31 00:16:51 -0500

    Fetch user posted images when viewing profile

--- React/smartbrain/ui/src/components/Profile/ProfileSummary.js
+++ React/smartbrain/ui/src/components/Profile/ProfileSummary.js
@@ -1,29 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Logo from '../Logo/Logo';
 import { Card } from 'reactstrap';
 import { connect } from 'react-redux';
+import { fetchPostedImages } from '../../actions';
 
-const ProfileSummary = ({ user }) => {
+const ProfileSummary = ({ user, fetchPostedImages }) => {
 
     if (!user) {
         return <div>Loading...</div>
     }
 
+    useEffect(() => {
+        fetchPostedImages();
+    }, []);
+
     return (
-        <div className="profile flex">
-            <Logo />
-            <Card className="br3 ba b--black-10 w-80 mw8 shadow-5 center mr-4">
-                <main className="pa4 black-80 flex">
-                    <div className="w-50">
-                        <img src={user.profile_image} className="br-100 ba h3 w3 dib" alt="avatar" />
-                        <h1>{user.name}</h1>
-                    </div>
-                    <div className="w-50 py-5">
-                        <h4>{`Images Submitted: ${user.entries}`}</h4>
-                        <p>{`Member Since: ${new Date(user.joined).toLocaleDateString()}`}</p>
-                    </div>
-                </main>
-            </Card>
+        <div className="profile">
+            <div className="flex">
+                <Logo />
+                <Card className="br3 ba b--black-10 w-80 mw8 shadow-5 center mr-4">
+                    <main className="pa4 black-80 flex">
+                        <div className="w-50">
+                            <img src={user.profile_image} className="br-100 ba h3 w3 dib" alt="avatar" />
+                            <h1>{user.name}</h1>
+                        </div>
+                        <div className="w-50 py-5">
+                            <h4>{`Images Submitted: ${user.entries}`}</h4>
+                            <p>{`Member Since: ${new Date(user.joined).toLocaleDateString()}`}</p>
+                        </div>
+                    </main>
+                </Card>
+            </div>
+            <div className="images">
+
+            </div>
         </div>
     );
 }
@@ -34,4 +44,4 @@ const mapStateToProps = state => {
     }
 };
 
-export default connect(mapStateToProps)(ProfileSummary);
+export default connect(mapStateToProps, { fetchPostedImages })(ProfileSummary);
