bda5b92c36fc9843ec580921805fddb1c343c73c
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-01-31 12:20:23 -0500

    Render Posted Images

--- React/smartbrain/ui/src/components/Profile/ProfileSummary.js
+++ React/smartbrain/ui/src/components/Profile/ProfileSummary.js
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react';
-import Logo from '../Logo/Logo';
-import { Card } from 'reactstrap';
 import { connect } from 'react-redux';
+import { Card } from 'reactstrap';
+import ProfileHeader from './ProfileHeader';
 import { fetchPostedImages } from '../../actions';
 
-const ProfileSummary = ({ user, fetchPostedImages }) => {
+const ProfileSummary = ({ user, images, fetchPostedImages }) => {
 
     if (!user) {
         return <div>Loading...</div>
@@ -14,25 +14,21 @@ const ProfileSummary = ({ user, fetchPostedImages }) => {
         fetchPostedImages();
     }, []);
 
+    const renderPostedImages = () => {
+        return images.map((image) => {
+            return (
+                <Card key={image.id} className="w-30 px-2 py-2 m-2">
+                    <img  src={image.url} alt="" />
+                </Card>
+            );
+        });
+    };
+
     return (
         <div className="profile">
-            <div className="flex">
-                <Logo />
-                <Card className="br3 ba b--black-10 w-80 mw8 shadow-5 center mr-4">
-                    <main className="pa4 black-80 flex">
-                        <div className="w-50">
-                            <img src={user.profile_image} className="br-100 ba h3 w3 dib" alt="avatar" />
-                            <h1>{user.name}</h1>
-                        </div>
-                        <div className="w-50 py-5">
-                            <h4>{`Images Submitted: ${user.entries}`}</h4>
-                            <p>{`Member Since: ${new Date(user.joined).toLocaleDateString()}`}</p>
-                        </div>
-                    </main>
-                </Card>
-            </div>
-            <div className="images">
-
+            <ProfileHeader user={user} />
+            <div className="images flex m-3">
+                {renderPostedImages()}
             </div>
         </div>
     );
@@ -40,7 +36,8 @@ const ProfileSummary = ({ user, fetchPostedImages }) => {
 
 const mapStateToProps = state => {
     return {
-        user: state.user
+        user: state.user,
+        images: state.profile.images || []
     }
 };
 
