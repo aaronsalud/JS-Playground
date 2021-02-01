93cd8244d5da872ed2a394c0ddd4213b94bdbdeb
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-02-01 13:25:19 -0500

    Remove modal state

--- React/smartbrain/ui/src/reducers/index.js
+++ React/smartbrain/ui/src/reducers/index.js
@@ -2,11 +2,9 @@ import { combineReducers } from 'redux';
 import imageReducer from './imageReducer';
 import userReducer from './userReducer';
 import profileReducer from './profileReducer';
-import modalReducer from './modalReducer';
 
 export default combineReducers({
     user: userReducer,
     image: imageReducer,
-    modal: modalReducer,
     profile: profileReducer
 });
