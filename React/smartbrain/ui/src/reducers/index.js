5ec305f401865ff33bdeb5783ce3b18339b3adbe
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2021-02-01 23:00:39 -0500

    Configure error state

--- React/smartbrain/ui/src/reducers/index.js
+++ React/smartbrain/ui/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import imageReducer from './imageReducer';
 import userReducer from './userReducer';
 import profileReducer from './profileReducer';
+import errorReducer from './errorReducer';
 
 export default combineReducers({
     user: userReducer,
     image: imageReducer,
-    profile: profileReducer
+    profile: profileReducer,
+    error: errorReducer
 });
